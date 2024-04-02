c847953ab94794691ec2c3645bf865002534cfb2
Jacob Riggs <jarigg@iu.edu>
2024-04-01 21:40:37 -0400

    Started Kotlin tutorial

--- Kotlin/KotlinTesting/src/Main.kt
+++ Kotlin/KotlinTesting/src/Main.kt
@@ -1,3 +1,44 @@
 fun main() {
     println("Hello World!")
+
+    val name = "Mary"
+    val age = 20
+
+    println("$name is $age years old")
+
+    var customers = 10
+
+    customers = 8
+
+    customers = customers + 3
+    customers += 7
+    customers -= 3
+    customers *= 2
+    customers /= 3
+
+    println(customers)
+
+    val g: Int
+
+    g = 3
+
+    val h: String = "hello"
+
+    println(g)
+    println(h)
+
+    val a: Int = 100
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+
+    val readOnlyShapes = listOf("triangle", "square", "circle")
+    println(readOnlyShapes)
+
+    val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+    println(shapes)
+
+    
 }
