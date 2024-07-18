a9dc6a8e225ebdb746e10fbb29e9b5baac5229d1
Jacob Riggs <jarigg@iu.edu>
2024-07-18 18:45:03 -0400

    Started Rust basics

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, world!");
+
+    let name = "Jacob";
+    let age = 22;
+    let alive = true;
+
+    println!("My name is {}, I am {} years old, and {}", name, age, alive);
+
+    if age > 18 {
+        println!("You are an adult");
+    }
+    else {
+        println!("You are not an adult");
+    }
+
+    for x in 1..11{
+        if x < 10 {
+            print!("{}, ",x);
+        }
+        else {
+            print!("{}",x);
+        }
+    }
 }
